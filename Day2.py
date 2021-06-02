d6861e684152dd418fb03641f3f8112a7876a82d
RICHU PATEL <82597620+Richupatel2109@users.noreply.github.com>
2021-06-02 21:13:10 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,44 @@
+#print("Hello world...")
+#n1,n2,name=10,20.5,"Richu"
+#n1=10
+#n2=10+2j
+#name="Richu"
+#n1=n2=name=10
+#print(n1)
+#print("n2 is",n2,type(n2))
+##print("name is",name)
+
+#name="Richu"
+
+#print(name)
+#print(name[2])
+#print(name[1:5])
+#print(name[3:])
+#print(name[:4])
+#print(name * 2)
+#print("name is " +name)
+
+#l1=[10,20,30.5,'richu']
+#print(l1)
+#print(type(l1))
+#print(l1[1])
+#print(l1[1:3])
+#print(l1[2:])
+#print(l1[:3])
+
+#l1=[10,20,30.5,'richu']
+#l1[1]=20.5
+#print(l1)
+#print(type(l1))
+#print(l1[1])
+#print(l1[1:3])
+#print(l1[2:])
+#print(l1[:3])
+
+d1={10:'hello','a1':100,20:200}
+print(d1)
+print(type(d1))
+print(d1[10])
+print(d1['a1'])
+print(d1[20])
+
---
+++ Day3.py
@@ -0,0 +1,88 @@
+#name=input('Enter name: ')
+#print('Name is',name)
+
+
+
+#CONDITION STATEMENTS
+
+#n1=int(input('enter num1:'))
+#n2=int(input('enter num2:'))
+#ans=n1+n2
+#print('sum is',ans)
+
+
+#n1=10
+#n2=20
+#if n1>n2:
+   #print('n1 is greatest') 
+#if n2>n1:
+   #print('n2 is greatest') 
+
+
+#n1=10
+#n2=20
+#if n1>n2:
+   #print('n1 is greatest') 
+#else:
+   #print('n2 is greatest') 
+   
+
+
+#n1=20
+#n2=200
+#if n1==n2:
+   #print('both numbers are equal') 
+#elif n1>n2:
+   #print('n1 is greatest') 
+#else:
+   #print('n2 is greatest') 
+
+
+#n1=-10
+#if n1>=0:
+    #if n1==0:
+      #print('n1 is zero')
+    #else:
+        #print('n1 is positive')
+#else:
+    #print('n1 is negative')
+
+
+
+#LOOPS STATEMENTS
+
+#i=1
+#while i<=10:
+    #print('value is',i)
+    #i +=1
+
+
+#for i in 'hello':
+    #print('value is',i)
+
+#l1=[10,20,'hello']
+#print(l1)
+
+#for i in l1:
+    #print('value is ',i)
+
+#for i in range(10):
+    #print('value is',i)
+
+#for j in range(1,5):
+    #print('value is',j)
+
+#for k in range(1,10,2):
+    #print('value is',k)
+
+l1=[10,20,'hello']
+print(l1)
+for i in l1:
+   print('value is ',i)
+
+for j in range(len(l1)):
+   print('value is ',l1[j])
+
+else:
+   #print('loop ended..')
+   pass
