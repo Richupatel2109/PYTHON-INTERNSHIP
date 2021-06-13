c5ff6a4a04de17ef77c2051c9554141ea448f12a
RICHU PATEL <patelrs2109@gmail.com>
2021-06-13 18:06:19 +0530

    changes in day 7

--- Day7/Myapp/views.py
+++ Day7/Myapp/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('HELLO WORLD')
+
+def aboutus(request):
+    return HttpResponse('About Us')
+
+def contactus(request):
+    return HttpResponse('Contact Us')
